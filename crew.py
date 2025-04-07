66551ee2d96169bab8e8535897c46012326585e6
Sanjukta Chatterjee <158772512+Sanjukta-Chatterjee@users.noreply.github.com>
2025-04-08 01:29:33 +0530

    Add files via upload

---
+++ crew.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from crewai import Crew
+from agents import property_researcher, property_analyst
+from tasks import research_task, analysis_task
+
+crew = Crew(
+    agents=[property_researcher],#, property_analyst], 
+    tasks=[research_task],#, analysis_task], 
+    verbose=True
+    )
+
+task_output = crew.kickoff()
+print(task_output)
